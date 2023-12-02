7b6f6fc5831fbc02d88d63a7d77090d1fbbe20e7
asav-abhish <abhishekbarnwal1000@gmail.com>
2023-12-03 01:11:11 +0530

    Initial commit

---
+++ src/Spotify.js
@@ -0,0 +1,27 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/"; // Make sure this matches your Spotify Application's redirect URI
+const clientId = "865fc463917e4f489a5e3def600fda57";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce((initial, item) => {
+        let parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+        return initial;
+      }, {});
+  };
+  
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
